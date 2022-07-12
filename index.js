1b97c975391ed0488152b691b8d400d4d9e7ac1a
SorayaBoolean <sorayakhyabaniassl@gmail.com>
2022-07-12 22:18:26 +0200

    refactoring

--- index.js
+++ index.js
@@ -3,21 +3,25 @@
 const parola = prompt ('Inserisci una parola');
 console.log (parola);
 
-//2.Funzione per capire se la parola inserita è palindroma:
-    //2a. Scomponi parola data da User in Array
 
-    const arrayParola = parola.split ("");
-    console.log (arrayParola);
+//2. Scomponi parola data da User in Array
 
-//3 Scomposto l' array va confrontato con Array al contrario (palindromo)
+    const array1 = parola.split ("");
+    console.log (array1);
 
-const paroleReversed = arrayParola.reverse();
-console.log(reversed);
+//3. Scomposto l' array va confrontato con Array al contrario (palindromo)
+
+
+
+
+
 
    
 
     
-
+//4.Funzione per capire se la parola inserita è palindroma:
   
-   
+  function check_array1_array2 () {
+    //blocco di codice
+  } 
 
