bce3b90652a9bdae0f4cb3c73d8e2536082be39e
SorayaBoolean <sorayakhyabaniassl@gmail.com>
2022-07-12 17:21:20 +0200

    refactoring: deleted cicle

--- index.js
+++ index.js
@@ -4,9 +4,17 @@ const parola = prompt ('Inserisci una parola');
 console.log (parola);
 
 //2.Funzione per capire se la parola inserita è palindroma:
-    //2a. Crea variabile dove inserire la parola da scomporre
-    //3b. Scomposto l' array confrontarlo con un ciclo con la 'parola' inserita
+    //2a. Scomponi parola data da User in Array
 
     const arrayParola = parola.split ("");
     console.log (arrayParola);
+
+    //2b. Scomposto l' array confrontarlo con un ciclo con la 'parola' inserita è palindroma
+
+   
+
+    
+
+  
+   
 
