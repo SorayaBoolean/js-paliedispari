2e2f0bdddc5f455362fe13f4bfcf0f05b1a13e4d
SorayaBoolean <sorayakhyabaniassl@gmail.com>
2022-07-13 00:03:14 +0200

    function ready for check palindrome words or not palindrome

--- index.js
+++ index.js
@@ -3,35 +3,7 @@
   const parola = prompt ('Inserisci una parola');
   console.log (parola);
 
-
-//2. Scomponi parola data da User in Array
-
-  const array1 = parola.split ("");
-  console.log ( " original array is " + array1);
-
-//3. Crea un array di 'parola' al contrario 
-
-  const array2 = []
-    
-  for ( let i = array1.length ; i>= 0 ; i--) {
-
-    array2.push (array1[i]);
-
-  }
-  console.log ( " reversed array is " + array2);
-
-  //ricorstuisci array al contrario in una stringa
-
-  const array2_Stringa = array2.join('');
-  console.log (array2_Stringa)
-
-  //check che ilconfronto tra i due array sia palindroma  con ciclo
-
-  if (parola == array2_Stringa ) {
-    alert ('la parola è palindroma')
-  } else {
-    alert ('la parola NON è palindroma')
-  }
+palindroma (parola, ) ;
 
     
 //4.Funzione per capire se la parola inserita è palindroma:
@@ -39,18 +11,30 @@
   function palindroma () {
   
 
+  const array1 = parola.split ("");
+  console.log ( " original array is " + array1);
+
+
+  const array2 = []
+    
+  for ( let i = array1.length ; i>= 0 ; i--) {
+    array2.push (array1[i]);
+  }
+  console.log ( " reversed array is " + array2);
+
+
+  const array2_Stringa = array2.join('');
+  console.log (array2_Stringa)
+
+
+  if (parola == array2_Stringa ) {
+    alert ('la parola è palindroma')
+  } else {
+    alert ('la parola NON è palindroma')
+  }
 
 
 
-
-
-
-
-
-
-
-
-
-    return true
+    return 
   } 
 
